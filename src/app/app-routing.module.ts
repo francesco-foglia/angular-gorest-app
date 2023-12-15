4cdba0d531e4a900c3b3eb719f2630ec7370553a
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-15 17:16:14 +0100

    Add AuthGuard to protect routes and redirect unauthenticated users to the login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './auth/auth.guard';
+
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsersComponent } from './components/users/users.component';
@@ -10,12 +12,14 @@ import { PostsComponent } from './components/posts/posts.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '', component: DashboardComponent, children: [
+    path: '', component: DashboardComponent, canActivate: [AuthGuard], children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
       { path: 'users', component: UsersComponent, },
       { path: 'users/:id', component: UserComponent },
       { path: 'posts', component: PostsComponent },
     ]
-  }
+  },
+  { path: '**', redirectTo: 'users' }
 ];
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    } else if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/users']);
+    }
   }
 
 }
