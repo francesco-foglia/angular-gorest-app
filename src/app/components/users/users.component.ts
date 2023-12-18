bacbce0851aa8bdffadc46ad2cbc08a17d06f8a1
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-18 01:08:03 +0100

    Update navbar styling and add users confirmation/error messages

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  confirmMessage: string = '';
+  errorMessage: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 import { NavbarModule } from '../navbar/navbar.module';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [UsersComponent],
   imports: [
     CommonModule,
     UsersRoutingModule,
-    NavbarModule
+    NavbarModule,
+    MatCardModule
   ]
 })
 export class UsersModule { }
