5497922ed325e252ba4c2a48096fc4b7951e40bc
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-22 23:35:17 +0100

    Replace Angular Material imports with MaterialModule import

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -3,25 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MaterialModule } from '../../modules/material/material.module';
+
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatProgressSpinnerModule
+    MaterialModule
   ]
 })
 export class LoginModule { }
