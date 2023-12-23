3c576a002cfa2cb4f55365c4f87f2eef2b407f0d
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-23 23:12:53 +0100

    Update Angular Material imports

--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
-import { MaterialModule } from '../../modules/material/material.module';
+import { MaterialModule } from '../../modules/material.module';
 import { NavbarModule } from '../navbar/navbar.module';
 import { MessageModule } from '../message/message.module';
 import { SpinnerModule } from '../spinner/spinner.module';
