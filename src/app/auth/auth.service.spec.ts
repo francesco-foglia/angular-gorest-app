e05cfe80c8ee9a313e9da097726505a0a9eee165
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-24 19:24:01 +0100

    Initialize isLoggedIn based on sessionStorage value

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -15,17 +15,13 @@ describe('AuthService', () => {
     expect(authService).toBeTruthy();
   });
 
-  it('should initialize isLoggedIn to false if not set in sessionStorage', () => {
-    expect(authService.isLoggedIn).toBe(false);
-  });
-
-  it('should initialize isLoggedIn to true if set to true in sessionStorage', () => {
+  it('should initialize isLoggedIn to true if sessionStorage returns "true"', () => {
     spyOn(sessionStorage, 'getItem').and.returnValue('true');
     authService = TestBed.inject(AuthService);
     expect(authService.isLoggedIn).toBe(true);
   });
 
-  it('should initialize isLoggedIn to false if set to false in sessionStorage', () => {
+  it('should initialize isLoggedIn to false if sessionStorage returns "false"', () => {
     spyOn(sessionStorage, 'getItem').and.returnValue('false');
     authService = TestBed.inject(AuthService);
     expect(authService.isLoggedIn).toBe(false);
@@ -33,6 +29,12 @@ describe('AuthService', () => {
 
   it('should initialize isLoggedIn to false if sessionStorage returns an invalid value', () => {
     spyOn(sessionStorage, 'getItem').and.returnValue('invalid');
+    authService = TestBed.inject(AuthService);
+    expect(authService.isLoggedIn).toBe(false);
+  });
+
+  it('should initialize isLoggedIn to false if sessionStorage returns null', () => {
+    spyOn(sessionStorage, 'getItem').and.returnValue(null);
     authService = TestBed.inject(AuthService);
     expect(authService.isLoggedIn).toBe(false);
   });
