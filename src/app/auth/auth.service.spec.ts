0aaa50e08b0dc5982d9f580c2d5f248e5e123f20
Francesco Foglia <francescofogliajj@gmail.com>
2023-12-24 19:00:01 +0100

    Add tests for AuthService

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+      providers: [AuthService]
+    });
+    authService = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
+  });
+
+  it('should initialize isLoggedIn to false if not set in sessionStorage', () => {
+    expect(authService.isLoggedIn).toBe(false);
+  });
+
+  it('should initialize isLoggedIn to true if set to true in sessionStorage', () => {
+    spyOn(sessionStorage, 'getItem').and.returnValue('true');
+    authService = TestBed.inject(AuthService);
+    expect(authService.isLoggedIn).toBe(true);
+  });
+
+  it('should initialize isLoggedIn to false if set to false in sessionStorage', () => {
+    spyOn(sessionStorage, 'getItem').and.returnValue('false');
+    authService = TestBed.inject(AuthService);
+    expect(authService.isLoggedIn).toBe(false);
+  });
+
+  it('should initialize isLoggedIn to false if sessionStorage returns an invalid value', () => {
+    spyOn(sessionStorage, 'getItem').and.returnValue('invalid');
+    authService = TestBed.inject(AuthService);
+    expect(authService.isLoggedIn).toBe(false);
   });
 });
